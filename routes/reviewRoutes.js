dff7bef4f1ba849809a04e93657293a63274e74a
Jimmy Frank <166641378+jimmmyfrank@users.noreply.github.com>
2026-09-19 00:03:44 +0200

    Add trader review routes and update imports

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const { createReview, getReviews } = require('../controllers/reviewController');
+const { createReview, getReviews,
+      createTraderReview,
+  getTraderReviews,
+  deleteTraderReview,
+ } = require('../controllers/reviewController');
 const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/', auth, createReview);
-router.get('/', getReviews); // Public with query params
+router.get('/', getReviews);
+ // Public with query params
+ router.get('/trader/:traderId', getTraderReviews);
+router.post('/trader/:traderId', auth, createTraderReview);
+router.delete('/trader/:id', auth, deleteTraderReview);
+
 
 module.exports = router;
